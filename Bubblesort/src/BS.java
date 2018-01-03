7f794cf483ed5d9460bd61b289b349d58c63282d
vinetis9 <>
2018-01-03 14:44:01 +0100

    ...

---
+++ Bubblesort/src/BS.java
@@ -0,0 +1,22 @@
+class BubbleSort{
+
+    public static<T extends Comparable<T>> void BS(T array[]){
+
+
+
+
+    }
+
+
+    public static void main(String[] args){
+
+
+
+
+    }
+
+
+
+
+
+        }
