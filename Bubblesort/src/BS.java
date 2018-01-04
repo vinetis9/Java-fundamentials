107a3790f8e2b15d31fc9b17e7dccfa24b088c14
vinetis9 <>
2018-01-04 21:02:02 +0100

    ..

--- Bubblesort/src/BS.java
+++ Bubblesort/src/BS.java
@@ -1,7 +1,24 @@
 class BubbleSort{
 
     public static<T extends Comparable<T>> void BS(T array[]){
+        boolean swap;
 
+        for(int i=0;i<=array.length;i++){
+
+            for(int j = i; j<=array.length;i++){
+
+                int comp = array[i].compareTo(array[j]);
+                T temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
+
+
+            }
+
+
+
+
+        }
 
 
 
